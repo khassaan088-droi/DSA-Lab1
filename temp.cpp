8a5b0bf7ae989bbafed4dc9cc9014acc7ba1f3f5
khassaan088-droi <khassaan088@gmail.com>
2026-09-08 16:52:20 +0500

    Task 5 Done

--- temp.cpp
+++ temp.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 using namespace std;
-
-int naiveSearch(const char text[], const char pattern[]) {
-    if (pattern[0] == '\0') 
-     return 0;
-    for (int i = 0; text[i] != '\0'; i++) {
-        int j = 0;
-        while (text[i + j] == pattern[j] && pattern[j] != '\0') {
-            j++;
-        }
-        if (pattern[j] == '\0') return i;
+void printPascalsTriangle(int n) {
+    if (n <= 0) {
+        cout << "Empty triangle\n";
+        return;
     }
+    int triangle[20][20]; 
 
-    return -1;
+    for (int i = 0; i < n; i++) {
+        
+        triangle[i][0] = 1;
+        triangle[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+        }
+
+       
+        cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j <= i; j++) {
+            cout << triangle[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
-
 int main() {
-    cout << naiveSearch("hello world", "hello") << endl; 
-    cout << naiveSearch("hello world", "world") << endl; 
-    cout << naiveSearch("hello world", "cpp") << endl;   
-    cout << naiveSearch("hello world", "") << endl;      
+    cout << " Test 1: n = 0 ---\n";
+    printPascalsTriangle(0);
+    cout << "Test 2: n = 1 ---\n";
+    printPascalsTriangle(1);
+    cout << " Test 3: n = 5 ---\n";
+    printPascalsTriangle(5);
+
     return 0;
 }
